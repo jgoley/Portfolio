ec6a65633e976c3cb8e0338bd6bc435123fcd1c1
jgoley <jgoley@gmail.com>
2014-11-06 10:40:00 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@
 
 var gulp = require('gulp');
 
+var deploy = require('gulp-gh-pages');
+
+gulp.task('deploy', function () {
+    return gulp.src('./dist/**/*')
+        .pipe(deploy(options));
+});
+
 // load plugins
 var $ = require('gulp-load-plugins')();
 
