fa5c48a02b0c2e66e511ea54e056a5ee07cd8956
jgoley <jgoley@gmail.com>
2014-11-19 03:03:34 -0500

    initial commit

---
+++ app/js/fn.js
@@ -0,0 +1,30 @@
+function showWork(e, w) {
+    e.preventDefault();
+    $('.more-work').removeClass('showing');
+    $('#' + w + '-container').addClass('showing');
+    scrollToElement($('#' + w + '-container'));
+}
+
+function scrollToElement($element) {
+    $('html, body').animate({
+        scrollTop: $element.offset().top - 100
+    }, 1000);
+}
+
+$.fn.serializeObject = function() {
+    return this.serializeArray().reduce(function(acum, i) {
+        acum[i.name] = i.value;
+        return acum;
+    }, {});
+};
+
+function sendMail(params) {
+    Parse.Cloud.run('sendEmail', params, {
+        success: function(result) {
+            console.log(result)
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
