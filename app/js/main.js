7a289abe7d002a93ba24e0946d59560516c6dcaa
jgoley <jgoley@gmail.com>
2014-12-11 19:37:36 -0500

    Fixes typo

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,3 @@
-console.log("Hey, How's it going?  Need a developer? jgoley@gmail.com or @jgoley");
-
 $('.main-nav').onePageNav({
     currentClass: 'current',
     changeHash: false,
@@ -37,3 +35,7 @@ $('.videos-link').on('click', function(e) {
 $('.graphics-link').on('click', function(e) {
     showWork(e, 'graphics');
 });
+
+try {
+    console.log('%c    ___        ________   ________ \n    |\\  \\      |\\   ____\\ |\\   __  \\ \n    \\ \\  \\     \\ \\  \\___| \\ \\  \\|\\  \\ \n  __ \\ \\  \\     \\ \\  \\  ___\\ \\  \\\\\\  \\ \n |\\  \\\\_\\  \\  ___\\ \\  \\|\\  \\\\ \\  \\\\\\  \\   \n \\ \\________\\|\\__\\\\ \\_______\\\\ \\_______\\ \n  \\|________|\\|__| \\|_______| \\|_______| \n\n %cHey, How\'s it going?. Need a developer? \n\n github: http://github.com/jgoley \n twitter: http://twitter.com/jgoley \n ', 'color:rgba(1, 164, 245, 1);', 'color:red;')
+    } catch(e) {}
