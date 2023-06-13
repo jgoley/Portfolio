3ead3ff1ce86864a3f0317dcb2a28192df28d025
Jonathan Golightly <jgoley@gmail.com>
2023-06-12 22:31:11 -0400

    Dust off, update and fix gulp config

--- app/scripts/components/header.js
+++ app/scripts/components/header.js
@@ -1,77 +1,78 @@
 class HeaderNavItem extends React.Component {
   isActive(url) {
-    let path = getPagePath()
-    let title =  this.props.item.pageTitle.replace(' ', '-').toLowerCase()
+    let path = getPagePath();
+    let title = this.props.item.pageTitle.replace(" ", "-").toLowerCase();
     if (path.includes(title)) {
-      return 'active'
-    } else
-      return ''
+      return "active";
+    } else return "";
   }
 
   relativePath() {
-    let path = getPagePath()
-    let relativePath = ""
-    path = path.match(/\//g)
-    if (typeof path === 'object' && path.length > 1)
-      path.pop()
-      path.forEach(()=> relativePath += '../')
-    return relativePath += this.props.item.url
+    let path = getPagePath();
+    let relativePath = "";
+    path = path.match(/\//g);
+    if (typeof path === "object" && path.length > 1) path.pop();
+    path.forEach(() => (relativePath += "../"));
+    return (relativePath += this.props.item.url);
   }
 
   render() {
-    let item = this.props.item
-    return(
+    let item = this.props.item;
+    return (
       <li className={this.isActive()}>
         <a href={this.relativePath()}>
           <span>{item.pageTitle}</span>
         </a>
       </li>
-    )
+    );
   }
 }
 
 class HeaderNav extends React.Component {
   show() {
-    if (getPagePath() !== '/') return true
-    else return false
+    if (getPagePath() !== "/") return true;
+    else return false;
   }
 
   renderChildren() {
-    let children = []
+    let children = [];
     let navItems = [
-      {url: 'web-developer.html', pageTitle: 'Web Developer'},
-      {url: 'photographer/', pageTitle: 'Photographer'},
-      {url: 'videographer.html', pageTitle: 'Videographer'},
-      {url: 'designer.html', pageTitle: 'Designer'}
-    ]
-    navItems.forEach((item)=> {
-      children.push(<HeaderNavItem item={item} />)
-    })
-    return children
+      { url: "web-developer.html", pageTitle: "Web Developer" },
+      { url: "photographer/", pageTitle: "Photographer" },
+      { url: "videographer.html", pageTitle: "Videographer" },
+      { url: "designer.html", pageTitle: "Designer" },
+    ];
+    navItems.forEach((item) => {
+      children.push(<HeaderNavItem item={item} />);
+    });
+    return children;
   }
 
   render() {
     if (this.show()) {
-      return(
+      return (
         <nav id="main-nav">
           <button class="show-menu"></button>
           <ul>
             {this.renderChildren()}
-            <li className='exit' />
+            <li className="exit" />
           </ul>
         </nav>
-      )
-    } else
-      return null
+      );
+    } else return null;
   }
 }
 
 class Header extends React.Component {
   render() {
-    return ([
-      <a href="/"><span class="badge"></span></a>,
-      <a href="/"><h1 class="jgo-name">Jonathan Goley</h1></a>,
-      <HeaderNav />
-    ])
+    return [
+      <a href="/">
+        <span class="badge"></span>
+      </a>,
+      <a href="/">
+        <h1 class="jgo-name">Jonathan Goley</h1>
+      </a>,
+      <HeaderNav />,
+    ];
   }
 }
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,169 +1,171 @@
 // generated on 2017-02-13 using generator-gulp-webapp 1.1.1
-import fs from 'fs';
-import path from 'path';
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import browserSync from 'browser-sync';
-import del from 'del';
-import {stream as wiredep} from 'wiredep';
-import concat from 'gulp-concat'
-import concatMulti from 'gulp-concat-multi'
-import minify from 'gulp-minify'
-import deploy from 'gulp-gh-pages'
-import babel from 'gulp-babel'
-import debug from 'gulp-debug'
-import tap from 'gulp-tap'
-import cleanCSS from 'gulp-clean-css'
+import fs from "fs-extra";
+import path from "path";
+import browserSync from "browser-sync";
+import del from "del";
+import gulp from "gulp";
+// import { stream as wiredep } from "wiredep";
+import concatMulti from "gulp-concat-multi";
+import deploy from "gulp-gh-pages";
+import babel from "gulp-babel";
+import cleanCSS from "gulp-clean-css";
+import stylus from "gulp-stylus";
+import sourcemaps from "gulp-sourcemaps";
+import plumber from "gulp-plumber";
+import useref from "gulp-useref";
+import pug from "gulp-pug";
+import uglify from "gulp-uglify";
+import react from "gulp-react";
+// import gulpIf from "gulp-if";
+import imagemin from "gulp-imagemin";
+import cache from "gulp-cache";
+import watch from "gulp-watch";
 
-const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
+const { src, dest, series, parallel } = gulp;
+
+const browsersync = browserSync.create();
+
 const options = {};
-const scriptsPath = 'app/scripts'
-
+const scriptsPath = "app/scripts";
 
 function getFolders(dir) {
-  return fs.readdirSync(dir).filter(function(file) {
+  return fs.readdirSync(dir).filter(function (file) {
     return fs.statSync(path.join(dir, file)).isDirectory();
   });
 }
 
-gulp.task('deploy', ['build'], () => {
-  return gulp.src('dist/**/*')
-    .pipe(deploy(options));
-});
+// console.log({ reload });
 
-gulp.task('styles', () => {
-  return gulp.src('app/styles/main.styl')
-    .pipe($.sourcemaps.init())
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
-    .pipe($.stylus({
-      paths: ['.']
-    }))
+const _deploy = (cb) => {
+  src("dist/**/*").pipe(deploy(options));
+};
+
+const styles = (cb) => {
+  src("app/styles/main.styl")
+    .pipe(sourcemaps.init())
+    .pipe(
+      stylus({
+        paths: ["."],
+      })
+    )
     .pipe(cleanCSS())
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/styles"))
+    .pipe(browserSync.reload({ stream: true }));
+  cb();
+};
 
-gulp.task('scripts', () => {
-  let pageScriptsPath = `${scriptsPath}/page_scripts`
+const scripts = (cb) => {
+  let pageScriptsPath = `${scriptsPath}/page_scripts`;
   getFolders(pageScriptsPath).forEach((dir) => {
-    return concatMulti(
-        {
-          [`${dir}.js`]: [
-            `${scriptsPath}/vendor/*.js`,
-            `${scriptsPath}/components/*.js`,
-            `${scriptsPath}/*.js`,
-            `${pageScriptsPath}/${dir}/**/*.js`,
-          ]
-        }
-      )
-      .pipe($.plumber())
-      .pipe($.react())
-      .pipe(babel({presets: ['es2015']}))
-      .pipe($.uglify())
-      .pipe(gulp.dest('dist/scripts/'))
-      .pipe(reload({stream: true}));
-  })
-});
-
-gulp.task('views', () => {
-  return gulp.src('app/**/*.pug')
-    .pipe($.useref())
-    .pipe($.plumber())
-    .pipe($.pug({pretty: false}))
-    .pipe(gulp.dest('dist/'))
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
-    .pipe($.if($.if.isFile, $.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    }))
-    .on('error', function (err) {
-      console.log(err);
-      this.end();
-    })))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html',
-    '!app/*.pug',
-    'app/CNAME'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
-
-gulp.task('serve', ['views', 'styles', 'scripts','images', 'fonts'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist', 'app'],
-      routes: {
-        '/bower_components': 'bower_components',
-        '/node_modules': 'node_modules'
-      }
-    }
+    return concatMulti({
+      [`${dir}.js`]: [
+        `${scriptsPath}/vendor/*.js`,
+        `${scriptsPath}/components/*.js`,
+        `${scriptsPath}/*.js`,
+        `${pageScriptsPath}/${dir}/**/*.js`,
+      ],
+    })
+      .pipe(plumber())
+      .pipe(react())
+      .pipe(babel({ presets: ["@babel/env"] }))
+      .pipe(uglify())
+      .pipe(dest("dist/scripts/"))
+      .pipe(browserSync.reload({ stream: true }));
   });
+  cb();
+};
 
-  gulp.watch([
-    'app/**/*.pug',
-    '.tmp/styles/**/*.styl',
-    '.tmp/scripts/**/*.js',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
+const views = () =>
+  src("app/**/*.pug")
+    .pipe(useref())
+    .pipe(plumber())
+    .pipe(pug({ pretty: false }))
+    .pipe(dest("dist/"))
+    .pipe(browsersync.reload({ stream: true }));
 
-  gulp.watch('app/**/*.pug', ['views']);
-  gulp.watch('app/styles/**/*.styl', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/images/**/*.js', ['images']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
-});
+const images = () => {
+  return src("app/images/**/*")
+    .pipe(
+      cache(
+        imagemin({
+          progressive: true,
+          interlaced: true,
+          // don't remove IDs from SVGs, they are often used
+          // as hooks for embedding and styling
+          svgoPlugins: [{ cleanupIDs: false }],
+        })
+      ).on("error", function (err) {
+        console.log(err);
+        this.end();
+      })
+    )
+    .pipe(dest("dist/images"));
+};
 
-gulp.task('serve:dist', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
+const fonts = () => {
+  return src("**/*.{eot,svg,ttf,woff,woff2}")
+    .pipe(dest(".tmp/fonts"))
+    .pipe(dest("dist/fonts"));
+};
+
+const extras = () => {
+  return src(["app/*.*", "!app/*.html", "!app/*.pug", "app/CNAME"], {
+    dot: true,
+  }).pipe(dest("dist"));
+};
+
+export const build = parallel(styles, scripts, views, images, fonts, extras);
+
+export const clean = (cw) => {
+  del.bind(null, [".tmp", "dist"]);
+  cw();
+};
+
+const browserSyncReload = (cb) => {
+  browsersync.reload();
+  cb();
+};
+
+const browserSyncTask = (cb) => {
+  browsersync.init({
     server: {
-      baseDir: ['dist']
-    }
+      baseDir: "./dist",
+    },
   });
-});
+  cb();
+};
+
+export const _watch = () => {
+  watch(
+    [
+      "app/**/*.pug",
+      "app/images/**/*",
+      "app/scripts/**/*.js",
+      "app/styles/**/*.styl",
+    ],
+    series(styles, scripts, views, images, fonts, extras, browserSyncReload)
+  );
+};
+
+export const serve = series(
+  styles,
+  scripts,
+  views,
+  images,
+  fonts,
+  extras,
+  browserSyncTask,
+  _watch
+);
 
 // inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build', ['views', 'styles', 'scripts','images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
-gulp.task('default', ['clean'], () => {
-  gulp.start('build');
-});
+// task("wiredep", () => {
+//   src("app/*.html")
+//     .pipe(
+//       wiredep({
+//         ignorePath: /^(\.\.\/)*\.\./,
+//       })
+//     )
+//     .pipe(dest("app"));
+// });
