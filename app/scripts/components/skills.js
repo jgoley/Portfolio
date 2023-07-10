f4fc0eb0e3058e840c8ed5c82f00c70f839ed829
Jonathan Golightly <jgoley@gmail.com>
2023-07-09 22:32:27 -0400

    Update web dev page with some current projects

--- app/scripts/components/skills.js
+++ app/scripts/components/skills.js
@@ -1,14 +1,12 @@
-class Skills extends React.Component {
-  render() {
-    return (
-      <div className='skills'>
-        <h1>Skills</h1>
-        <p>
-          <span>[</span>
-          <span> { this.props.skills.join(", ") } </span>
-          <span>]</span>
-        </p>
-      </div>
-    )
-  }
-}
+const Skills = ({ skills }) => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <p>
+        <span>[</span>
+        <span> {skills.join(", ")} </span>
+        <span>]</span>
+      </p>
+    </div>
+  );
+};
